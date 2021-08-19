8fafc575371e6924f181221d36f90487e445975a
Josh W Mahnesmith <jwm2166@gmail.com>
2021-08-19 14:01:37 -0500

    Added Game Path Management
    Added a way to determine where the steam location is and the beatsaber file path.
    
    Also added Unit tests for the prior added functionality.

---
+++ BeatDownloaderConsole/GamePathManager.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BeatDownloaderConsole
+{
+    public class GamePathManager
+    {
+
+        public const string beatSaberAppID = "620980";
+
+        public string GetSteamPath()
+        {
+            string thirdyTwoBitKey = @"HKEY_LOCAL_MACHINE\SOFTWARE\Valve\Steam";
+            string sixtyFourBitKey = @"HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Valve\Steam";
+
+            // Try and get values for both key paths
+            string thirdyTwoBitSteamPath = "";
+            string sixtyFourBitSteamPath = "";
+            try
+            {
+                thirdyTwoBitSteamPath = (string)Registry.GetValue(thirdyTwoBitKey, "InstallPath", null);
+                sixtyFourBitSteamPath = (string)Registry.GetValue(sixtyFourBitKey, "InstallPath", null);
+
+                if (!(thirdyTwoBitSteamPath == null))
+                {
+                    return thirdyTwoBitSteamPath;
+                }
+                else if (!(sixtyFourBitSteamPath == null))
+                {
+                    return sixtyFourBitSteamPath;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return "";
+        }
+
+        public string GetGamePath()
+        {
+            string key = @"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Steam App " + beatSaberAppID;
+
+            try
+            {
+                return (string) Registry.GetValue(key, "InstallLocation", null);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
